ad08c2fea42e10fa72878be87f4db8b7885ddcb9
Hebert Henrique Barboza de Brito <hebert.hb.brito@gmail.com>
2025-03-24 09:48:04 -0300

    feat: corrigir botão de download e adicionar currículo nos arquivos public

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -41,7 +41,13 @@ const About = () => {
                     </ul>
 
                     <div className="mt-8">
-                        <Buttom link={"#"} inf={"Baixar CV"}/>
+                        <a
+                            href="/Hebert.pdf"
+                            download
+                            className="bg-dourado-texto text-backgroud-buttom py-2 px-6 rounded-full text-xl font-bold transition-all duration-300 transform hover:scale-105 hover:bg-dourado-texto-dark"
+                        >
+                            Baixar CV
+                        </a>
                     </div>
                 </div>
             </div>
